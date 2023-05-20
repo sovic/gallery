553be2930acec4c2fd7c2e4badff493797275904
Vojtěch Bojda <info@vojtechbojda.cz>
2023-05-20 23:43:45 +0200

    adds findGalleriesCovers

--- src/Repository/GalleryItemRepository.php
+++ src/Repository/GalleryItemRepository.php
@@ -10,7 +10,7 @@ use Sovic\Gallery\Entity\GalleryItem;
 
 class GalleryItemRepository extends EntityRepository
 {
-    private function initQueryBuilder(Gallery $gallery): QueryBuilder
+    private function getGalleryQueryBuilder(Gallery $gallery): QueryBuilder
     {
         $qb = $this->getEntityManager()->createQueryBuilder();
         $qb->select('gi')
@@ -27,9 +27,23 @@ class GalleryItemRepository extends EntityRepository
         return $qb;
     }
 
+    private function getBatchQueryBuilder(string $model, array $modelIds): QueryBuilder
+    {
+        $qb = $this->getEntityManager()->createQueryBuilder();
+        $qb->select('gi');
+        $qb->from(GalleryItem::class, 'gi');
+        $qb->andWhere('gi.model = :model');
+        $qb->setParameter(':model', $model);
+        $qb->andWhere('gi.modelId IN (:model_id)');
+        $qb->setParameter(':model_id', $modelIds);
+        $qb->andWhere('gi.isTemp = 0');
+
+        return $qb;
+    }
+
     public function findByGallery(Gallery $gallery, ?int $offset = null, ?int $limit = null): array
     {
-        $qb = $this->initQueryBuilder($gallery);
+        $qb = $this->getGalleryQueryBuilder($gallery);
         if ($offset) {
             $qb->setFirstResult($offset);
         }
@@ -42,16 +56,24 @@ class GalleryItemRepository extends EntityRepository
 
     public function findGalleryCoverImage(Gallery $gallery): ?GalleryItem
     {
-        $qb = $this->initQueryBuilder($gallery);
+        $qb = $this->getGalleryQueryBuilder($gallery);
         $qb->orderBy('gi.isCover', 'DESC');
         $qb->setMaxResults(1);
 
         return $qb->getQuery()->getSingleResult();
     }
 
+    public function findGalleriesCovers(string $model, array $modelIds): array
+    {
+        $qb = $this->getBatchQueryBuilder($model, $modelIds);
+        $qb->andWhere('gi.isCover = 1');
+
+        return $qb->getQuery()->getResult();
+    }
+
     public function findGalleryHeroImage(Gallery $gallery): ?GalleryItem
     {
-        $qb = $this->initQueryBuilder($gallery);
+        $qb = $this->getGalleryQueryBuilder($gallery);
         // TODO single item, multiple variants
         $qb->andWhere($qb->expr()->orX('gi.isHero = 1', 'gi.isHeroMobile = 1'));
         $qb->setMaxResults(1);
