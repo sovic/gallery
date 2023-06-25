a7d82132443a94007e9c4599aa2b059371e21b89
Vojtěch Bojda <info@vojtechbojda.cz>
2023-06-25 15:17:42 +0200

    comment out unfinished methods

--- src/Gallery/Gallery.php
+++ src/Gallery/Gallery.php
@@ -74,98 +74,98 @@ class Gallery extends AbstractEntityModel
 
         return (new GalleryItemResultSet($items));
     }
-
-    public function getVideo(): ?array
-    {
-//        $expr = $this->entityManager->getExpressionBuilder();
-//        $qb = $this->initQueryBuilder('video');
-//        $qb->andWhere(
-//            $qb->expr()->orX(
-//                $expr->isNotNull('gi.name'),
-//                $expr->isNotNull('gi.description')
-//            )
-//        );
-//        $qb->orderBy('gi.sequence', 'ASC');
-//        /** @var GalleryItem $item */
-//        $items = $qb->getQuery()->getResult();
-//        if (count($items) <= 0) {
-//            return null;
-//        }
-//        $item = $items[0];
-//        if ($item->getDescription() && Text::isUrl($item->getDescription())) {
-//            $url = $item->getDescription();
-//        } elseif ($item->getName() || $item->getDescription()) {
-//            $filename = $item->getName() ?: $item->getDescription();
-//            $url = '/dl/' . $item->getId() . '/' . $filename . '.' . $item->getExtension();
-//        } else {
-//            return null;
-//        }
 //
-//        return [
-//            'url' => $url,
-//        ];
-
-        return null;
-    }
-
-    public function getDocs(): array
-    {
-//        $expr = $this->entityManager->getExpressionBuilder();
-//        $qb = $this->initQueryBuilder('reading');
-//        $qb->andWhere(
-//            $qb->expr()->orX(
-//                $expr->isNotNull('gi.name'),
-//                $expr->isNotNull('gi.description')
-//            )
-//        );
-//        $qb->orderBy('gi.sequence', 'ASC');
-//        $items = $qb->getQuery()->getResult();
-//        $result = [];
-//        /** @var GalleryItem $item */
-//        foreach ($items as $item) {
-//            $description = $item->getDescription();
-//            if ($description && Text::isUrl($description)) {
-//                $name = File::publicFileName(basename($item->getExtension()));
-//                $url = $item->getDescription();
-//            } elseif ($description || $item->getName()) {
-//                $filename = !empty($item->getName()) ? $item->getName() : $description;
-//                $name = File::publicFileName($filename, $item->getExtension());
-//                $url = '/dl/' . $item->getId() . '/' . $filename . '.' . $item->getExtension();
-//            } else {
-//                continue;
-//            }
+//    public function getVideo(): ?array
+//    {
+////        $expr = $this->entityManager->getExpressionBuilder();
+////        $qb = $this->initQueryBuilder('video');
+////        $qb->andWhere(
+////            $qb->expr()->orX(
+////                $expr->isNotNull('gi.name'),
+////                $expr->isNotNull('gi.description')
+////            )
+////        );
+////        $qb->orderBy('gi.sequence', 'ASC');
+////        /** @var GalleryItem $item */
+////        $items = $qb->getQuery()->getResult();
+////        if (count($items) <= 0) {
+////            return null;
+////        }
+////        $item = $items[0];
+////        if ($item->getDescription() && Text::isUrl($item->getDescription())) {
+////            $url = $item->getDescription();
+////        } elseif ($item->getName() || $item->getDescription()) {
+////            $filename = $item->getName() ?: $item->getDescription();
+////            $url = '/dl/' . $item->getId() . '/' . $filename . '.' . $item->getExtension();
+////        } else {
+////            return null;
+////        }
+////
+////        return [
+////            'url' => $url,
+////        ];
 //
-//            $result[] = [
-//                'name' => $name,
-//                'url' => $url,
-//            ];
-//        }
+//        return null;
+//    }
 //
-//        return $result;
-        return [];
-    }
-
-    public function getDownloads(): array
-    {
-//        $qb = $this->initQueryBuilder('downloads');
-//        $qb->orderBy('gi.sequence', 'ASC');
-//        $items = $qb->getQuery()->getResult();
+//    public function getDocs(): array
+//    {
+////        $expr = $this->entityManager->getExpressionBuilder();
+////        $qb = $this->initQueryBuilder('reading');
+////        $qb->andWhere(
+////            $qb->expr()->orX(
+////                $expr->isNotNull('gi.name'),
+////                $expr->isNotNull('gi.description')
+////            )
+////        );
+////        $qb->orderBy('gi.sequence', 'ASC');
+////        $items = $qb->getQuery()->getResult();
+////        $result = [];
+////        /** @var GalleryItem $item */
+////        foreach ($items as $item) {
+////            $description = $item->getDescription();
+////            if ($description && Text::isUrl($description)) {
+////                $name = File::publicFileName(basename($item->getExtension()));
+////                $url = $item->getDescription();
+////            } elseif ($description || $item->getName()) {
+////                $filename = !empty($item->getName()) ? $item->getName() : $description;
+////                $name = File::publicFileName($filename, $item->getExtension());
+////                $url = '/dl/' . $item->getId() . '/' . $filename . '.' . $item->getExtension();
+////            } else {
+////                continue;
+////            }
+////
+////            $result[] = [
+////                'name' => $name,
+////                'url' => $url,
+////            ];
+////        }
+////
+////        return $result;
+//        return [];
+//    }
 //
-//        $results = [];
-//        /** @var GalleryItem $item */
-//        foreach ($items as $item) {
-//            $mediaPaths = GalleryHelper::getMediaPaths($item, $this->baseUrl, [GalleryHelper::SIZE_FULL]);
-//            $result = [
-//                'name' => File::publicFileName($item->getName(), $item->getExtension()),
-//                'filename' => $item->getName() . '.' . $item->getExtension(),
-//                'url' => $mediaPaths[GalleryHelper::SIZE_FULL],
-//            ];
-//            $results[] = $result;
-//        }
-//
-//        return $results;
-        return [];
-    }
+//    public function getDownloads(): array
+//    {
+////        $qb = $this->initQueryBuilder('downloads');
+////        $qb->orderBy('gi.sequence', 'ASC');
+////        $items = $qb->getQuery()->getResult();
+////
+////        $results = [];
+////        /** @var GalleryItem $item */
+////        foreach ($items as $item) {
+////            $mediaPaths = GalleryHelper::getMediaPaths($item, $this->baseUrl, [GalleryHelper::SIZE_FULL]);
+////            $result = [
+////                'name' => File::publicFileName($item->getName(), $item->getExtension()),
+////                'filename' => $item->getName() . '.' . $item->getExtension(),
+////                'url' => $mediaPaths[GalleryHelper::SIZE_FULL],
+////            ];
+////            $results[] = $result;
+////        }
+////
+////        return $results;
+//        return [];
+//    }
 
     /**
      * @param string $path
@@ -246,7 +246,7 @@ class Gallery extends AbstractEntityModel
     private function getGalleryStoragePath(): string
     {
         /** @noinspection SpellCheckingInspection */
-        $hash = md5($this->getEntity()->getId() . 'T3zmR34Swh4FZAA');
+        $hash = md5($this->getEntity()->getId() . 'T3zmR34Swh4FZAA'); // TODO config salt
         $path = str_split(substr($hash, 0, 6));
         $path[] = $hash;
 
